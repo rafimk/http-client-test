a25ae7275ef5002b93e739e2d316b52cb659e303
rafimk <rafi.genius.cs@gmail.com>
2022-08-10 20:16:52 +0400

    first commit

---
+++ MembershipCardDto.cs
@@ -0,0 +1,14 @@
+﻿namespace http_client_test;
+
+public class MembershipCardDto
+{
+    public string MembershipNo { get; set;}
+    public string Date { get; set; }
+    public string FullName { get; set;}
+    public string District { get; set;}
+    public string Mandalam { get; set; }
+    public string Panchayath { get; set;}
+    public string State { get; set; }
+    public string Area { get; set;}
+    public string CollectedBy { get; set;}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+using http_client_test;
+
+Console.WriteLine("Hello, World!");
+
+var membershipCardDto = new MembershipCardDto
+{
+    MembershipNo = "AUH10001",
+    Date = "01/01/2022",
+    FullName = "MUHAMMED RAFI",
+    District = "KASARAGOAD",
+    Mandalam = "TRIKARPUR",
+    Panchayath = "TRIKARPUR",
+    State = "ABU DHABI",
+    Area = "MUSAFFAH",
+    CollectedBy = "AGENT"
+};
+
+JsonContent content = JsonContent.Create(membershipCardDto);
+
+HttpClient _httpClient = new HttpClient();
+var response = await _httpClient.PostAsync("http://localhost:5003/reports/filestream", content);
+
+if (response.IsSuccessStatusCode)
+{
+    var mybytearray = response.Content.ReadAsByteArrayAsync().Result;
+} 
+
